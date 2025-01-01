1fb2dee116037563e4683926996901f895673962
hfao <150105438+hfao@users.noreply.github.com>
2025-01-02 01:06:52 +0700

    code conflic dung keo

--- app/Http/Controllers/Client/CartController.php
+++ app/Http/Controllers/Client/CartController.php
@@ -3,41 +3,81 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Carts;
+use App\Models\CartDetail;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Cart;
+use Darryldecode\Cart\Cart as CartCart;
 
 class CartController extends Controller
 {
-    public function checkout(){
-        return view('client.pages.cart.checkout');
+    public function cartList()
+    {
+        $cartItems = Carts::with('cartDetail')->get();
+        // Debug để xem dữ liệu
+        // dd([
+        //     'cartItems' => $cartItems->toArray(),
+        //     'first_item' => $cartItems->first(),
+        //     'cart_detail' => $cartItems->first()->cartDetail
+        // ]);
+
+        return view('client.pages.cart.cartPopup', ['cartItems' => $cartItems]);
     }
 
+    public function addToCart(Request $request, $id, $quantity){
 
-    public function cart(){
-        $cartCollection = Cart::getContent();
-        // dd($cartCollection);
-        return view('client.pages.cart.cartPopup', [
-            'cartCollection' => $cartCollection
-        ]);
-    }
-    
-    public function addToCart($id, $quantity){
-        
         $product = Product::find($id);
+        $imageCustom = $request->input('image_custom');
+        $price = $request->input('price');
+        // $priceGift = $request->query('price_gift', $product->price_gift);
+        // 'price_gift' => $priceGift,
+
+        if (!$product) {
+            return redirect('/404');
+        }
 
         Cart::add(array(
-            'id' => $product->id, // inique row ID
+            'id' => $product->id,
             'name' => $product->name,
-            'price' => $product->price,
+            'price' => $price,
             'quantity' => $quantity,
-            'attributes' => array(
-            'image' => $product->image
-            )
+            'options' => [
+                'image_custom' => $imageCustom,
+            ]
         ));
+
 
         return redirect()->route('client.cart');
     }
+
+    public function cart(){
+        $cartCollection = Cart::getContent();
+        $item_cart = Carts::all();
+        // dd($item_cart);
+        // dd($cartCollection);
+        return view('client.pages.cart.cartPopup', [
+            'cartCollection' => $cartCollection,
+            'item_cart' => $item_cart,
+        ]);
+    }
+
+    // public function addToCart($id, $quantity){
+
+    //     $product = Product::find($id);
+
+    //     Cart::add(array(
+    //         'id' => $product->id, // inique row ID
+    //         'name' => $product->name,
+    //         'price' => $product->price,
+    //         'quantity' => $quantity,
+    //         'attributes' => array(
+    //         'image' => $product->image
+    //         )
+    //     ));
+
+    //     return redirect()->route('client.cart');
+    // }
 
     public function cartDelete($id){
 
@@ -55,33 +95,35 @@ class CartController extends Controller
                 'relative' => false,
                 'value' => $quantity
             )
-          ));
+        ));
 
-        return response()->json([ 
-            'status' =>200
-           
-           ]
+        return response()->json([
+                'status' =>200
+
+            ]
         );
     }
+
     public function checkoutvnpay_payment(){
-       
-        
+
+        $product = Product::all();
+        // dd($product);
         $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
         $vnp_Returnurl = "http://127.0.0.1:8000/client/thanh-toan";
-        $vnp_TmnCode = "QDNBN1PJ";//Mã website tại VNPAY 
+        $vnp_TmnCode = "QDNBN1PJ";//Mã website tại VNPAY
         $vnp_HashSecret = "H5PAZUPT8EDRWWC6GDN2WTONJHXV8D4Q"; //Chuỗi bí mật
-        
-        $vnp_TxnRef = '1000'; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
-        $vnp_OrderInfo = 'Thanh toan don hang test';
+
+        $vnp_TxnRef = 'AHCD'; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
+        $vnp_OrderInfo = 'Thanh toan';
         $vnp_OrderType = 'billpayment';
-        $vnp_Amount = 20000 * 100;
+        $vnp_Amount = ' ' . $product->attributes->price;
         $vnp_Locale = 'vn';
         $vnp_BankCode = 'NCB';
         $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
         //Add Params of 2.0.1 Version
-        
+
         //Billing
-       
+
         $inputData = array(
             "vnp_Version" => "2.1.0",
             "vnp_TmnCode" => $vnp_TmnCode,
@@ -96,14 +138,14 @@ class CartController extends Controller
             "vnp_ReturnUrl" => $vnp_Returnurl,
             "vnp_TxnRef" => $vnp_TxnRef
         );
-        
+
         if (isset($vnp_BankCode) && $vnp_BankCode != "") {
             $inputData['vnp_BankCode'] = $vnp_BankCode;
         }
         if (isset($vnp_Bill_State) && $vnp_Bill_State != "") {
             $inputData['vnp_Bill_State'] = $vnp_Bill_State;
         }
-        
+
         //var_dump($inputData);
         ksort($inputData);
         $query = "";
@@ -118,21 +160,30 @@ class CartController extends Controller
             }
             $query .= urlencode($key) . "=" . urlencode($value) . '&';
         }
-        
+
         $vnp_Url = $vnp_Url . "?" . $query;
         if (isset($vnp_HashSecret)) {
-            $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret);//  
+            $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret);//
             $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
         }
         $returnData = array('code' => '00'
-            , 'message' => 'success'
-            , 'data' => $vnp_Url);
-            if (isset($_POST['redirect'])) {
-                header('Location: ' . $vnp_Url);
-                die();
-            } else {
-                echo json_encode($returnData);
-            }
-            // vui lòng tham khảo thêm tại code demo
+        , 'message' => 'success'
+        , 'data' => $vnp_Url);
+        if (isset($_POST['redirect'])) {
+            header('Location: ' . $vnp_Url);
+            die();
+        } else {
+            echo json_encode($returnData);
+        }
+        // vui lòng tham khảo thêm tại code demo
+    }
+
+
+
+    public function showcart(){
+
+        $cart_item = Carts::all();
+
+        return view('client.pages.cart.cart',['cart_item' => $cart_item]);
     }
 }
--- app/Http/Controllers/Client/ClientProductController.php
+++ app/Http/Controllers/Client/ClientProductController.php
@@ -18,88 +18,88 @@ class ClientProductController extends Controller
 
         $category = Category::findOrFail($id);
         $category_list =  Category::with('product')
-        ->where('parent_id',0)
-        ->get();
+            ->where('parent_id',0)
+            ->get();
 
         $products = Product::with('category')->where('category_id',$id)->paginate(9);
 
-        
+
 
         return view('client.pages.product.shop',[
-            'category' => $category,
-            'category_list' => $category_list,
-            'products'=> $products,
-            'id' => $id,
-            
-        ]
-    );
+                'category' => $category,
+                'category_list' => $category_list,
+                'products'=> $products,
+                'id' => $id,
+
+            ]
+        );
     }
 
     public function productdetail($id){
 
         $products = Product::with('product_image','category')->where('id',$id)->first();
         $category_list =  Category::with('product')
-        ->where('parent_id',0)
-        ->get();
+            ->where('parent_id',0)
+            ->get();
         $products_related = Product::with('category')
-        ->where('category_id',$products->category->id)
-        ->where('id','!=',$products->id)
-        ->paginate(4);
+            ->where('category_id',$products->category->id)
+            ->where('id','!=',$products->id)
+            ->paginate(4);
         return view('client.pages.product.productdetail',[
             'products'=> $products,
             'products_related'=>$products_related,
             'category_list'=>$category_list,
             'id'=> $id,
-            
+
         ]);
     }
     // public function saveCustomImage(Request $request, $id, $cart_id) {
     //     try {
     //         $product = Product::findOrFail($id);
-    
+
     //         // Validate input
     //         $request->validate([
     //             'image' => 'required',
     //             'recipient_name' => 'required|string',
     //             'custom_message' => 'required|string',
     //         ]);
-    
+
     //         $imageData = $request->input('image');
-            
+
     //         // Tách dữ liệu base64
     //         list($type, $imageData) = explode(';', $imageData);
     //         list(, $imageData) = explode(',', $imageData);
     //         $imageData = base64_decode($imageData);
-    
+
     //         // Kiểm tra xem dữ liệu hình ảnh có hợp lệ không
     //         if ($imageData === false) {
     //             throw new \Exception('Dữ liệu hình ảnh không hợp lệ.');
     //         }
-    
+
     //         // Tạo tên file
     //         $fileName = 'product_' . $id . '_custom_' . time() . '.png';
-            
+
     //         // Đường dẫn tới thư mục custom_images trong public
     //         $path = public_path('custom_images');
-            
+
     //         // Kiểm tra và tạo thư mục nếu chưa tồn tại
     //         if (!file_exists($path)) {
     //             mkdir($path, 0777, true);
     //         }
-    
+
     //         // Lưu file
     //         file_put_contents($path . '/' . $fileName, $imageData);
-    
+
     //         // Đường dẫn để truy cập file
     //         $filePath = asset('custom_images/' . $fileName);
-    
+
     //         return response()->json([
     //             'success' => true,
     //             'message' => 'Lưu ảnh thành công',
     //             'file_name' => $fileName,
     //             'file_path' => $filePath,
     //         ]);
-    
+
     //     } catch (\Exception $e) {
     //         return response()->json([
     //             'success' => false,
@@ -109,68 +109,8 @@ class ClientProductController extends Controller
     //         ], 500);
     //     }
     // }
-//     public function uploadImage(Request $request, $id)
-// {
-//     $product = Product::findOrFail($id);
-
-//     // Validate input
-//     $request->validate([
-//         'image' => 'required',
-//         'recipient_name' => 'required|string',
-//         'custom_message' => 'required|string',
-//     ]);
-
-//     $imageData = $request->input('image');
-
-//     // Tách dữ liệu base64
-//     list($type, $imageData) = explode(';', $imageData);
-//     list(, $imageData) = explode(',', $imageData);
-//     $imageData = base64_decode($imageData);
-
-//     // Kiểm tra xem dữ liệu hình ảnh có hợp lệ không
-//     if ($imageData === false) {
-//         throw new \Exception('Dữ liệu hình ảnh không hợp lệ.');
-//     }
-
-//     // Tạo tên file
-//     $fileName = 'product_' . $id . '_custom_' . time() . '.png';
-
-//     // Đường dẫn tới thư mục custom_images trong public
-//     $path = public_path('custom_images');
-
-//     // Kiểm tra và tạo thư mục nếu chưa tồn tại
-//     if (!file_exists($path)) {
-//         mkdir($path, 0777, true);
-//     }
-
-//     // Lưu file
-//     file_put_contents($path . '/' . $fileName, $imageData);
-
-//     // Đường dẫn để truy cập file
-//     $filePath = asset('custom_images/' . $fileName);
-
-//     // Lưu vào bảng carts
-//     DB::table('carts')->insert([
-//         'image_custom' => $fileName, // Lưu tên file hình ảnh
-//         'cart_total' => 100000, // Ví dụ
-//         'cart_date' => now(), // Ví dụ
-//         'status' => 1, // Ví dụ
-//         'user_id' => Auth::user()->id, // Lấy ID người dùng hiện tại
-//         'payment_id' => 1, // Ví dụ
-//         'product_id' => $id, // Lấy ID sản phẩm
-//     ]);
-
-//     return response()->json(['success' => 'Hình ảnh đã được lưu thành công!']);
-// }
-public function uploadImage(Request $request, $id)
-{
-    try {
-        Log::info('Start uploading image', [
-            'product_id' => $id,
-            'has_image' => $request->has('image'),
-            'recipient_name' => $request->recipient_name
-        ]);
-
+    public function uploadImage(Request $request, $id)
+    {
         $product = Product::findOrFail($id);
 
         // Validate input
@@ -178,111 +118,100 @@ public function uploadImage(Request $request, $id)
             'image' => 'required',
             'recipient_name' => 'required|string',
             'custom_message' => 'required|string',
+            'recipientPrice' => 'required|integer',
+            'recipientEmail' => 'required|string',
         ]);
 
         $imageData = $request->input('image');
-        
-        // Log độ dài của dữ liệu ảnh
-        Log::info('Image data length: ' . strlen($imageData));
 
         // Tách dữ liệu base64
         list($type, $imageData) = explode(';', $imageData);
         list(, $imageData) = explode(',', $imageData);
         $imageData = base64_decode($imageData);
 
+        // Kiểm tra xem dữ liệu hình ảnh có hợp lệ không
         if ($imageData === false) {
-            throw new \Exception('Invalid image data');
+            throw new \Exception('Dữ liệu hình ảnh không hợp lệ.');
         }
 
         // Tạo tên file
-        $fileName = 'product_' . $id . '_' . time() . '.png';
+        $fileName = 'product_' . $id . '_custom_' . time() . '.png';
+
+        // Đường dẫn tới thư mục custom_images trong public
         $path = public_path('custom_images');
 
-        // Kiểm tra và tạo thư mục
+        // Kiểm tra và tạo thư mục nếu chưa tồn tại
         if (!file_exists($path)) {
             mkdir($path, 0777, true);
         }
 
-        // Kiểm tra quyền ghi
-        if (!is_writable($path)) {
-            throw new \Exception('Directory is not writable: ' . $path);
-        }
-
         // Lưu file
-        $fullPath = $path . '/' . $fileName;
-        if (file_put_contents($fullPath, $imageData) === false) {
-            throw new \Exception('Failed to save image file');
-        }
+        file_put_contents($path . '/' . $fileName, $imageData);
 
-        // Lưu vào database
-        DB::beginTransaction();
-        try {
-            $cart = DB::table('carts')->insert([
-                'image_custom' => $fileName,
-                'cart_total' => $product->price ?? 100000,
-                'cart_date' => now(),
-                'status' => 1,
-                'user_id' => Auth::id() ?? 1, // Fallback nếu không có user
-                'payment_id' => 1,
-                'product_id' => $id,
-                'created_at' => now(),
-                'updated_at' => now()
-            ]);
+        // Đường dẫn để truy cập file
+        $filePath = asset('custom_images/' . $fileName);
 
-            DB::commit();
-            $imageData = $request->input('image');
+        $money = $request->input('recipientPrice');
+        $email = $request->input('recipientEmail');
+        $image = $fileName;
 
-            // Tách dữ liệu base64
-            list($type, $imageData) = explode(';', $imageData);
-            list(, $imageData) = explode(',', $imageData);
-            $imageData = base64_decode($imageData);
+        $cartData = [
+            'image_custom' => $image,
+            'cart_total' => $money,
+            'cart_date' => now(),
+            'status' => 1,
+            'user_id' => 1,
+            'payment_id' => 1,
+            'product_id' => 1,
+        ];
 
-            // Tạo tên file
-            $fileName = 'product_' . $id . '_custom_' . time() . '.png';
+        $cartDetailData = [
+            'money' => $money,
+            'recipient_email' => $email,
+            'quantity' => 1,
+        ];
 
-            // Đường dẫn tới thư mục custom_images trong public
-            $path = public_path('custom_images');
 
-            // Kiểm tra và tạo thư mục nếu chưa tồn tại
-            if (!file_exists($path)) {
-                mkdir($path, 0777, true);
-            }
 
-            // Lưu file
-            file_put_contents($path . '/' . $fileName, $imageData);
+        session(['cart' => $cartData]);
+        session(['cart_detail' => $cartDetailData]);
 
-            // Đường dẫn để truy cập file
-            $filePath = asset('custom_images/' . $fileName);
+        // Lưu vào bảng carts
+        // $cartId = DB::table('carts')->insertGetId([
+        //     'image_custom' => $image, // Lưu tên file hình ảnh
+        //     'cart_total' => $money, // Ví dụ
+        //     'cart_date' => now(), // Ví dụ
+        //     'status' => 1, // Ví dụ
+        //     'user_id' => Auth::user()->id, // Lấy ID người dùng hiện tại
+        //     'payment_id' => 1, // Ví dụ
+        //     'product_id' => $id, // Lấy ID sản phẩm
+        // ]);
 
-            return response()->json([
-                'success' => true,
-                'message' => 'Lưu ảnh thành công',
-                'data' => [
-                    'file_name' => $fileName,
-                    'file_path' => asset('custom_images/' . $fileName)
-                ]
-            ]);
+        // DB::table('cart_detail')->insert([
+        //     'cart_id' => $cartId, // ID của cart vừa tạo
+        //     // 'product_id' => $id, // ID sản phẩm
+        //     'money' => $money, // Giá sản phẩm
+        //     'recipient_email' => $email,
+        //     'quantity' => 1,
+        // ]);
 
-        } catch (\Exception $e) {
-            DB::rollBack();
-            // Xóa file nếu lưu DB thất bại
-            if (file_exists($fullPath)) {
-                unlink($fullPath);
-            }
-            throw $e;
-        }
-
-    } catch (\Exception $e) {
-        Log::error('Error in uploadImage', [
-            'error' => $e->getMessage(),
-            'file' => $e->getFile(),
-            'line' => $e->getLine()
-        ]);
-
-        return response()->json([
-            'success' => false,
-            'message' => 'Có lỗi xảy ra: ' . $e->getMessage()
-        ], 500);
+        return response()->json(['success' => true]);
     }
-}
+    public function showCart()
+    {
+        // Lấy dữ liệu giỏ hàng từ session
+        $cart = session('cart');
+        $cartDetail = session('cart_detail');
+
+        // Kiểm tra xem dữ liệu giỏ hàng có tồn tại không
+        // if (!$cart || !$cartDetail) {
+        //     return redirect()->route('client.index');// Hiển thị view giỏ hàng trống nếu không có dữ liệu
+        // }
+
+        // Truyền dữ liệu giỏ hàng đến view
+        return view('client.pages.cart.cart', [
+            'cart' => $cart,
+            'cartDetail' => $cartDetail,
+        ]);
+    }
 }
